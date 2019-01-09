360933e8300306629951455c511a945e804f2322
mjrdnk <maciej.jordanek@gmail.com>
2019-01-09 21:02:17 +0100

    Adds jest with enzyme

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("App component", () => {
+  const wrapper = shallow(<App />);
+  it("Should render <App/>.", () => {
+    expect(wrapper).toBeTruthy();
+  });
+
+  it("Should contain <BrowserRouter/>.", () => {
+    expect(wrapper.find(<BrowserRouter />)).toBeTruthy();
+  });
 });
