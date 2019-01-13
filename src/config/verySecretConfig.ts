a6e58a5ef73cdc59608d6bca75e2c924fc9db965
mjrdnk <maciej.jordanek@gmail.com>
2019-01-13 19:49:37 +0100

    Updates config info

--- src/config/verySecretConfig.ts
+++ src/config/verySecretConfig.ts
@@ -2,6 +2,7 @@ export const verySecretConfig = {
   // !!! This is just for this demo purposes
   // This should not be here in any live system
   // Should be authenticated against some user auth api
+  // And token should be provided to be able to access other api enpoints/schemas
   USERNAME: "test",
   PASSWORD: "test123"
 };
